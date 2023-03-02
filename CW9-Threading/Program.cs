9a24777db5a50abadfbe980016b706758f99a907
Zachary Rose <zacerose@ut.utm.edu>
2023-03-02 13:06:50 -0600

    Got Thread class started.

--- CW9-Threading/Program.cs
+++ CW9-Threading/Program.cs
@@ -6,6 +6,27 @@ using System.Threading.Tasks;
 
 namespace CW9_Threading
 {
+    class FindPiThread
+    {
+        int dartsToThrow;
+        int dartsHit;
+        Random random;
+
+        FindPiThread(int dartsToThrow)
+        {
+            this.dartsToThrow = dartsToThrow;
+            dartsHit = 0;
+            random = new Random();
+        }
+        public int getDartsHit()
+        {
+            return dartsHit;
+        }
+        public void throwDarts()
+        {
+
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
