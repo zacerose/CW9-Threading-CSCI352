2d38d744ee112e70d0b40a694440bd1b7a80cdba
Zachary Rose <zacerose@ut.utm.edu>
2023-03-02 14:06:26 -0600

    Finished Main, no extra credit attempted yet.

--- CW9-Threading/Program.cs
+++ CW9-Threading/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CW9_Threading
@@ -12,7 +13,7 @@ namespace CW9_Threading
         int dartsHit;
         Random random;
 
-        FindPiThread(int dartsToThrow)
+        public FindPiThread(int dartsToThrow)
         {
             this.dartsToThrow = dartsToThrow;
             dartsHit = 0;
@@ -24,13 +25,53 @@ namespace CW9_Threading
         }
         public void throwDarts()
         {
-
+            double x, y;
+            for (int i = 0; i < dartsToThrow; i++)
+            {
+                x = random.NextDouble();
+                y = random.NextDouble();
+                if (x*x + y*y <= 1)
+                {
+                    dartsHit++;
+                }
+            }
         }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
+            int throws, threads;
+            Console.WriteLine("How many throws should each thread make: ");
+            throws = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("How many threads should be used: ");
+            threads = Int32.Parse(Console.ReadLine());
+
+            List<Thread> threadList = new List<Thread>();
+            List<FindPiThread> piThreads = new List<FindPiThread>();
+
+            for (int i = 0; i < threads; i++)
+            {
+                FindPiThread piThread = new FindPiThread(throws);
+                piThreads.Add(piThread);
+                Thread thread = new Thread(new ThreadStart(piThread.throwDarts));
+                threadList.Add(thread);
+
+                thread.Start();
+                Thread.Sleep(16);
+            }
+            foreach (Thread thread in threadList)
+            {
+                thread.Join();
+            }
+            int totalHits = 0;
+            foreach (FindPiThread piThread in piThreads)
+            {
+                totalHits += piThread.getDartsHit(); 
+            }
+
+            Console.WriteLine("Estimation of pi: " + (double)4 * totalHits / throws / threads);
+            Console.ReadKey();
         }
     }
 }
